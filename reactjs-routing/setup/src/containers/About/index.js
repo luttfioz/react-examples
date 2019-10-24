d24a4132d59f0c86437e876b438b7e3d86533647
Görkem Taşçı <gorkem_tasci@hotmail.com>
2019-10-24 15:45:29 +0300

    Routing done

---
+++ reactjs-routing/setup/src/containers/About/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Heading from '../../components/Heading'
+import Table from '../../components/Table'
+import Form from '../../components/Form'
+import * as data from '../../data.json';
+
+export default class About extends React.Component {
+    render() {
+		return <Heading title="About" />;
+	}
+}
---
+++ reactjs-routing/setup/src/containers/Home/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Heading from '../../components/Heading'
+import Table from '../../components/Table'
+import Form from '../../components/Form'
+import * as data from '../../data.json';
+
+class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			productList: data.productList
+		}
+		this.onDelete = this.onDelete.bind(this);
+		this.onAdd = this.onAdd.bind(this);
+	}
+
+	onDelete(index) {
+		let productList = this.state.productList;
+		productList.splice(index, 1);
+		this.setState({ productList });
+	}
+
+	onAdd(product) {
+		let productList = this.state.productList;
+		productList.push(product);
+		this.setState({ productList });
+    }
+    render() {
+		return (
+			<div className="home-container">
+				<Heading title="Products" />
+				<Table data={this.state.productList} onDelete={this.onDelete} />
+				<Form onSubmit={this.onAdd}/>
+			</div>
+		);
+	}
+
+}
+ 
+export default Home;
