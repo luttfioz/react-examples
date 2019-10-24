758f7c39f631d1b3ed62215bc1e3293c8a55944a
lutfioz <luttfioz@gmail.com>
2019-10-24 11:28:47 +0300

    Add react components props from json file

--- reactjs-props/setup/src/App.js
+++ reactjs-props/setup/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Heading from './components/Heading'
 import Table from './components/Table'
+import * as data from './data.json';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <Heading title="Products" />
-        <Table />
+        <Table data={data.productList}/>
       </div>
     );
   }
