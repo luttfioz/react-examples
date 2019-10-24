2e70202cac3223ef18445036e9fdebbee40f4079
lutfioz <luttfioz@gmail.com>
2019-10-24 14:16:37 +0300

    Add react components stateless component

--- reactjs-state/setup/src/components/Heading.js
+++ reactjs-state/setup/src/components/Heading.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-class Heading extends React.Component {
-    render() {
-        return <h1 className="text-center">{this.props.title}</h1>;
-    }
+const Heading = (props) => {
+    return <h1 className="text-center">{props.title}</h1>;
 }
 
 export default Heading;
+
+// Daha kısa hali
+// export default (props) => <h1 className="text-center">{props.title}</h1>;
