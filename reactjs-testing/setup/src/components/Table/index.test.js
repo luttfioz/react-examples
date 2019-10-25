825c280b7db296ff3404504c5f1006d8b2d2034b
lutfioz <luttfioz@gmail.com>
2019-10-25 14:54:09 +0300

    Add react testing

--- reactjs-testing/setup/src/components/Table/index.test.js
+++ reactjs-testing/setup/src/components/Table/index.test.js
@@ -1,5 +1,32 @@
 import React from 'react';
+import { shallow, mount } from 'enzyme';
+import Table from './index';
 
-describe('Table index tests: ', ()=>{
+import * as mockData from '../../data'
+import { IntlProvider } from 'react-intl';
+
+
+describe('Table tests without data: ', () => {
+    const wrapper = shallow(<Table />);
+    test('should render one TableHeader without error', () => {
+        expect(wrapper.find('TableHeader').length).toBe(1);
+    });
+    test('should render one TableBody without error', () => {
+        expect(wrapper.find('TableBody').length).toBe(1);
+    });
+})
+
+describe('Table tests with data: ', () => {
+    test('should render table row quantity correctly', () => {
+        const wrapper = mount(<IntlProvider><Table data={mockData.productList} /></IntlProvider>);
+        expect(wrapper.find('tr').length).toBe(4);
+    });
+    test('should call prop method on clicking delete icon', () => {
+        const onDelete = jest.fn();
+        const wrapper = mount(<IntlProvider><Table onDelete={onDelete} data={mockData.productList} /></IntlProvider>);
+        const deleteIcon = wrapper.find('.material-icons').at(0);
+        deleteIcon.simulate('click');
+        expect(onDelete).toBeCalledWith(0);
+    });
 
 })
