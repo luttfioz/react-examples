f9bf9bb352707715a0ec529a9cdb43bd85a29eb3
lutfioz <luttfioz@gmail.com>
2019-10-25 11:33:47 +0300

    Add react components redux and saga

--- reactjs-saga/setup/src/path.js
+++ reactjs-saga/setup/src/path.js
@@ -1,1 +1,2 @@
-export const API_PRODUCTS = "http://localhost:3000/api/products?code=111"
+export const API_PRODUCTS = "http://localhost:3000/api/products"
+//export const API_PRODUCTS = "http://localhost:3000/api/products?code=111" //for error
